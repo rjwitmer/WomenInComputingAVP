fe559ade9eb5a4a49e808370704e36e3ee849ac8
Bob Witmer <bwitmer@golden.net>
2025-08-10 09:40:54 -0400

    Initial Commit

---
+++ WomenInComputingAVP/WomenInComputingAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingAVPApp.swift
+//  WomenInComputingAVP
+//
+//  Created by Bob Witmer on 2025-08-10.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
